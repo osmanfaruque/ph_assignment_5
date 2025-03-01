e7bdca8927b2537399cd5d740ed584d516e2db54
osmanfaruque <faruqueosman01@gmail.com>
2025-03-01 23:40:15 +0600

    Clear History script added

--- assets/scripts/clear-history.js
+++ assets/scripts/clear-history.js
@@ -1,4 +1,3 @@
 document.getElementById("clear-history-btn").addEventListener("click", function() {
     document.getElementById("activity-log").innerHTML = "";
-    document.getElementById("activity-log").appendChild(emptyMessage);
 });
