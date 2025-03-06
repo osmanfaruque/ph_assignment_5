b1bd2200064328902885dda59b5560d298144756
osmanfaruque <faruqueosman0@gmail.com>
2025-03-07 02:34:06 +0600

    blog.html made responsive

--- assets/scripts/blogredirect.js
+++ assets/scripts/blogredirect.js
@@ -1,3 +1,7 @@
 document.getElementById("discover-btn").addEventListener("click", function() {
     window.location.href = "blog.html";
 });
+
+document.getElementById("discover-btn-sm").addEventListener("click", function() {
+    window.location.href = "blog.html";
+});
